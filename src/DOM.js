1fdd6b8691bfb49d7b4c5118104d4aa9a99b8de5
Tom Strat Laptop <tomstratfic@hotmail.com>
2020-02-19 20:02:52 +0000

    add init function and dom factory and style

--- src/DOM.js
+++ src/DOM.js
@@ -8,14 +8,25 @@ const DOM = (() => {
         mainContainer.id = "mainContainer";
         const topBar = document.createElement("div");
         topBar.id = "topBar";
+        topBar.innerHTML = "ToDoToDayToMorrow";
+        const dataContainer = document.createElement("div");
+        dataContainer.id = "dataContainer";
         const projectContainer = document.createElement("div");
         projectContainer.id = "projectContainer";
         const todoContainer = document.createElement("div");
         todoContainer.id = "todoContainer";
 
+        const todayTab = document.createElement("p");
+        todayTab.id = "todayTab";
+        todayTab.innerHTML = "Today";
+
+        projectContainer.appendChild(todayTab);
+
+        dataContainer.appendChild(projectContainer);
+        dataContainer.appendChild(todoContainer);
+
         mainContainer.appendChild(topBar);
-        mainContainer.appendChild(projectContainer);
-        mainContainer.appendChild(todoContainer);
+        mainContainer.appendChild(dataContainer);
 
         body.appendChild(mainContainer);
 
