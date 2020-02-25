f2ede8079acd92be3681f775a59d1e1e99c575e4
Tom Strat Laptop <tomstratfic@hotmail.com>
2020-02-25 22:05:57 +0000

    added projects add button and css

--- src/DOM.js
+++ src/DOM.js
@@ -16,11 +16,25 @@ const DOM = (() => {
         const todoContainer = document.createElement("div");
         todoContainer.id = "todoContainer";
 
-        const todayTab = document.createElement("p");
+        const todayTab = document.createElement("span");
         todayTab.id = "todayTab";
         todayTab.innerHTML = "Today";
+        const addProjectContainer = document.createElement("div");
+        addProjectContainer.id = "addProjectContainer";
+        const addProject = document.createElement("a");
+        addProject.id = "addProject";
+        addProject.innerHTML = "+";
+        const addProjectText = document.createElement("span");
+        addProjectText.id = "addProjectText";
+        addProjectText.innerHTML = "Projects";
+        const innerProjectContainer = document.createElement("div");
+        innerProjectContainer.id = "innerProjectContainer";
 
         projectContainer.appendChild(todayTab);
+        addProjectContainer.appendChild(addProject);
+        addProjectContainer.appendChild(addProjectText);
+        projectContainer.appendChild(addProjectContainer);
+        projectContainer.appendChild(innerProjectContainer);
 
         dataContainer.appendChild(projectContainer);
         dataContainer.appendChild(todoContainer);
