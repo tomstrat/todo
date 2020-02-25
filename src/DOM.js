52c8d3760663f238af4d81276d898ef8a296a195
Tom Strat Laptop <tomstratfic@hotmail.com>
2020-02-25 23:11:42 +0000

    added todo bar and css

--- src/DOM.js
+++ src/DOM.js
@@ -4,6 +4,7 @@ const DOM = (() => {
     const Init = () => {
         const body = document.querySelector("body");
 
+        //Main Containers
         const mainContainer = document.createElement("div");
         mainContainer.id = "mainContainer";
         const topBar = document.createElement("div");
@@ -16,6 +17,7 @@ const DOM = (() => {
         const todoContainer = document.createElement("div");
         todoContainer.id = "todoContainer";
 
+        //Projects Inner Container
         const todayTab = document.createElement("span");
         todayTab.id = "todayTab";
         todayTab.innerHTML = "Today";
@@ -30,11 +32,40 @@ const DOM = (() => {
         const innerProjectContainer = document.createElement("div");
         innerProjectContainer.id = "innerProjectContainer";
 
+        //Create Todo Bar
+        const createTodoContainer = document.createElement("div");
+        createTodoContainer.id = "createTodoContainer";
+        const todoBarContainer = document.createElement("div");
+        todoBarContainer.id = "todoBarContainer";
+        const todoSubmitContainer = document.createElement("div");
+        todoSubmitContainer.id = "todoSubmitContainer";
+        const todoInput = document.createElement("input");
+        todoInput.type = "text";
+        const todoDate = document.createElement("span");
+        todoDate.id = "todoDate";
+        todoDate.innerHTML = "Date"
+        const todoPriority = document.createElement("span");
+        todoPriority.id = "todoPriority";
+        const todoProject = document.createElement("span");
+        todoProject.id = "todoProject";
+        const todoSubmit = document.createElement("span");
+        todoSubmit.id = "todoSubmit";
+        todoSubmit.innerHTML = "ToDo";
+
         projectContainer.appendChild(todayTab);
         addProjectContainer.appendChild(addProject);
         addProjectContainer.appendChild(addProjectText);
         projectContainer.appendChild(addProjectContainer);
         projectContainer.appendChild(innerProjectContainer);
+
+        todoBarContainer.appendChild(todoInput);
+        todoBarContainer.appendChild(todoDate);
+        todoBarContainer.appendChild(todoPriority);
+        todoBarContainer.appendChild(todoProject);
+        todoSubmitContainer.appendChild(todoSubmit);
+        createTodoContainer.appendChild(todoBarContainer);
+        createTodoContainer.appendChild(todoSubmitContainer);
+        todoContainer.appendChild(createTodoContainer);
 
         dataContainer.appendChild(projectContainer);
         dataContainer.appendChild(todoContainer);
